a466f44c020d0e6979c345e11d7ab5f34a787160
beipeng <beipeng.peng@gmail.com>
2020-11-29 16:51:26 +0000

    initial commit

---
+++ src/envs/__init__.py
@@ -0,0 +1,12 @@
+from functools import partial
+from .particle import Particle
+from .multiagentenv import MultiAgentEnv
+from .mamujoco import MujocoMulti
+
+def env_fn(env, **kwargs) -> MultiAgentEnv: # TODO: this may be a more complex function
+    # env_args = kwargs.get("env_args", {})
+    return env(**kwargs)
+
+REGISTRY = {}
+REGISTRY["particle"] = partial(env_fn, env=Particle)
+REGISTRY["mujoco_multi"] = partial(env_fn, env=MujocoMulti)
